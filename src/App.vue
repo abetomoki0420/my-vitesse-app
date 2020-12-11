26a5375ff4d8ca7344a50bd5baf96c4379e16dab
abetomoki0420 <kimi.jack.of.all.trades@gmail.com>
2020-12-11 23:41:55 +0900

    build: initial

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="px-4 py-10 text-center bg-white">
+    <router-view />
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="text-xl mb-2">{{ count }}</div>
+    <div>
+      <button class="border border-indigo-500 text-indigo-500 rounded-md px-4 py-2 transition duration-500 ease select-none hover:text-white hover:bg-indigo-600 focus:outline-none focus:shadow-outline" @click="countUp">count up</button>
+    </div>
+    <div>
+      <button class="mt-2 border border-indigo-500 text-indigo-500 rounded-md px-4 py-2 transition duration-500 ease select-none hover:text-white hover:bg-indigo-600 focus:outline-none focus:shadow-outline" @click="clear">clear</button>
+    </div>
+    <div v-if="count % 2 !== 0">odd</div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref } from 'vue';
+
+const title = ref("My Vitesse App");
+const count = ref(0)
+
+const countUp = () => {
+  count.value++;
+}
+
+const clear = () => {
+  count.value = 0;
+}
+
+</script>
